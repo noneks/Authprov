76c4be838513092f2d5a45099c89f7092a0132c7
Feanaur <feanaur2193@gmail.com>
2014-01-21 12:24:18 +0700

    Initialize

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Authprov::Application.config.secret_key_base = 'aaa9fe6c8d401d4319280237e772bf79f7f16ba937c7e006970d1b4e0fc1d0b6a760182866908b5f2a14820ad8c5d6b05b2f0070560df56d977b64b84d3036bf'
