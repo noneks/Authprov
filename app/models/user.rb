ab3f854e91ee51a6a0bc93d869c330ec590ff7bc
Feanaur <feanaur2193@gmail.com>
2014-01-23 12:31:36 +0700

    User model: fixed vkontakte auth method

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,4 @@
 class User < ActiveRecord::Base
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :omniauthable
 
@@ -25,17 +23,18 @@ class User < ActiveRecord::Base
   end
 
 
-    def self.find_for_vkontakte_oauth (auth, signed_in_resource=nil)
-      user = User.where(:provider => auth.provider, :uid => auth.uid).first
-      if user 
-        return user
-      else 
-        user = User.create( :provider => access_token.provider,
-                            :url => access_token.info.urls.Vkontakte,
-                            :name => access_token.info.name,
-                            :email => access_token.extra.raw_info.domain+'@vk.com',
-                            :password => Devise.friendly_token[0,20])
-      end
+  def self.find_for_vkontakte_oauth access_token
+    user = User.where(:provider => access_token.provider, :uid => access_token.uid).first
+    if user 
+      return user
+    else 
+      user = User.create( provider: access_token.provider,
+                          uid: access_token.uid,
+                          name: access_token.info.name,
+                          email: access_token.uid+'@vk.com',
+                          password: Devise.friendly_token[0,20])
+    end
   end
   
 end
+
