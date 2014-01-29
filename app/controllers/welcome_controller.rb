f4c81175b40914c21b6aeb96db02e2708a876fd6
Kirill <kirill@kirill-1215P.(none)>
2014-01-29 14:05:27 +0700

    Looks like add paperclip #7

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,10 @@
 class WelcomeController < ApplicationController
   def index
   end
+
+  def update_user
+  	@user = User.find(params[:id])
+    @user.update_attribute(:picture, params[:user][:picture])
+  end
+
 end
