e41496e564e10022940e7db054f508cea5d95038
Kirill <kirill@kirill-1215P.(none)>
2014-01-29 14:15:20 +0700

    fix a lot

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -5,6 +5,7 @@ class WelcomeController < ApplicationController
   def update
   	@user = User.find(params[:id])
     @user.update_attribute(:picture, params[:user][:picture])
+    redirect_to users_index
   end
 
 end
