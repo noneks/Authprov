5a5203a4a1b6065653039802d76b336cd1d17dba
Kirill <kirill@kirill-1215P.(none)>
2014-01-31 10:28:25 +0700

    sad

--- db/schema.rb
+++ db/schema.rb
@@ -11,15 +11,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140128050412) do
+ActiveRecord::Schema.define(version: 20140129044011) do
+
+  create_table "images", force: true do |t|
+    t.string   "attach_file_name"
+    t.string   "attach_content_type"
+    t.integer  "attach_file_size"
+    t.string   "attach_updated_at"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
-    t.string   "email",                  default: "",   null: false
-    t.string   "encrypted_password",     default: "",   null: false
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,    null: false
+    t.integer  "sign_in_count",          default: 0,  null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
@@ -29,9 +38,11 @@ ActiveRecord::Schema.define(version: 20140128050412) do
     t.string   "provider"
     t.string   "uid"
     t.string   "name"
-    t.string   "image"
-    t.boolean  "admin",                  default: true
     t.integer  "roles_mask"
+    t.string   "picture_file_name"
+    t.string   "picture_content_type"
+    t.integer  "picture_file_size"
+    t.datetime "picture_updated_at"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
